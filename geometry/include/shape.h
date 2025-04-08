f4696a577f5e71266d50fc7f4e5c78bc12e288b2
Saakshi Kobarne <saakshik15@gmail.com>
2025-04-08 07:23:54 +0000

    Split the base shape class in Shape2D and Shape3D

--- geometry/include/shape.h
+++ geometry/include/shape.h
@@ -2,13 +2,29 @@
 #define SHAPE_H
 
 #include <string>
+#include <vector>
+#include "point2d.h"
+#include "point3d.h"
 
+// 🔹 Base abstract class
 class Shape {
 public:
     virtual ~Shape() = default;
-    
-    // Virtual method for saving points (overridden by each shape)
+
     virtual void savePointsToFile(const std::string& filename, bool append = false) = 0;
+
+    virtual std::vector<double> getParams() const = 0;
+    virtual std::string getType() const = 0;
+};
+
+class Shape2D : public Shape {
+public:
+    virtual std::vector<Point2D> getPoints() const = 0;
+};
+
+class Shape3D : public Shape {
+public:
+    virtual std::vector<Point3D> getPoints() const = 0;
 };
 
 #endif
