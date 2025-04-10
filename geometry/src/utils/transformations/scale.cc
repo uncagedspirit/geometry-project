84231825ce3e2837be92304747167d53f3b1a0df
Saakshi Kobarne <saakshik15@gmail.com>
2025-04-10 10:10:38 +0000

    Scaling logic

--- geometry/src/utils/transformations/scale.cc
+++ geometry/src/utils/transformations/scale.cc
@@ -1,10 +1,15 @@
-//dummy code for testing change later
-
-
 #include "transformer.h"
 #include <iostream>
+#include <array>
+#include <vector>
 
 std::vector<double> Transformer::scale(std::vector<double> params, double factor) {
-    std::cout << "[Dummy] Scaling with factor: " << factor << "\n";
-    return params;
+
+    std::vector<double> scaledParams;
+
+    scaledParams.reserve(params.size());
+    for (size_t i = 0; i < params.size(); ++i) {
+        scaledParams.push_back(params[i] * factor);
+    }
+    return scaledParams;
 }
